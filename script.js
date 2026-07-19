646ee8f31ad3a9bf05cd9d696443af77a503b28a
Subhasis1908 <subhadas7547@gmail.com>
2026-07-19 22:27:34 +0530

    Add addToCart function to manage shopping cart

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+// ===== TAJ BAKERY - STEP 3 (PART A) =====
 
+let cart = [];
+
+function addToCart(itemName, price) {
+    cart.push({
+        name: itemName,
+        price: price
+    });
+
+    alert(itemName + " added to cart!");
+
+    console.log(cart);
+}
